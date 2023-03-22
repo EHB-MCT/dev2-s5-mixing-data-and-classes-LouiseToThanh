c68f995b3b8c82af1503e9a9e4420f4f93ad70c8
LouiseToThanh <louise.to.thanh@student.ehb.be>
2023-03-22 16:26:36 +0100

    oefening classes

---
+++ js/Joke.js
@@ -0,0 +1,9 @@
+'use strict'; 
+
+export default class Jokes{
+    Constructor(category, setup, delivery){
+        this.category = category;
+        this.setup = setup; 
+        this.delivery = delivery; 
+    }
+   }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,34 @@
 "use strict";
 
+import Jokes from "./Joke";
+
 const app = {
+
     items: [],
+
     init() {
+        this.getData();
     },
+
     getData() {
+        fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10')
+        .then(function(jokes){
+            return jokes.json();
+        })
+       
+        .then(function(data){
+           console.log(data);
+        }) 
+       
+        
     },
+
     onSearch() {
     },
+
     render(message) {
     }
 
 }
+
 app.init()
